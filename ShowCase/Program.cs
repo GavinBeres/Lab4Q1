ead2113c00b404aca11a26179f6b45c35e4e20ea
Gavin Beresford <gberesf2@my.centennialcollege.ca>
2026-04-10 05:56:59 -0400

    Add project files.

---
+++ ShowCase/Program.cs
@@ -0,0 +1,13 @@
+﻿using UtilityLibraries;
+
+Console.Write("Enter a string: ");
+string? input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("No input provided.");
+}
+else
+{
+    Console.WriteLine($"Starts with uppercase? {input.StartsWithUpper()}");
+}
